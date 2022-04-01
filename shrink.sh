570c41d2447bebd1b249a897704dabb24796b763
Pepijn Olivier <olivier.pepijn@gmail.com>
2022-04-01 14:00:11 +0200

    Update shrink.sh

--- shrink.sh
+++ shrink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INPUTSIZE="940x529" #width x height
-OUTPUTSIZE="629x290" # width x height
+OUTPUTSIZE="629x290" # width x height. Or just define width, eg "629"
 
 COMPRESSION="80" # Define the lossy compression level. (30 is very light compression, 200 is heavy).
 
